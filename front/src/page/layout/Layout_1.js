56357b173189591047161e810d398a4900693c2e
zachjbrowning <zachjbrowning@outlook.com>
2020-05-23 21:13:04 +1000

    intermediately finished doc

--- front/src/page/layout/Layout_1.js
+++ front/src/page/layout/Layout_1.js
@@ -6,7 +6,7 @@ import SubHeader from '../SubHeader'
 export default function Layout_1(props) {
     return ( 
         <div id={('id' in props.section ? props.section['id'] : '')} className={"content-frame " + ('class' in props.section ? props.section['class'] : '')} style={'style' in props.section ? props.section['style'] : {}}>
-            <div className="container" style={'container' in props.section ? props.section['container'] : {}}>
+            <div className="container" style={'container-style' in props.section ? props.section['container-style'] : {}}>
                 {'header-text' in props.section ? <SubHeader header={props.section['header-text']} style={props.section['header-style']}/> : ''}
                 {'first' in props.section ? <Block block={props.section['first']}/> : ''}
             </div>
--- front/src/page/layout/Layout_2.js
+++ front/src/page/layout/Layout_2.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types'
 export default function Layout_2(props) {
     return (
         <div id={('id' in props.section ? props.section['id'] : '')} className={"content-frame " + ('class' in props.section ? props.section['class'] : '')} style={'style' in props.section ? props.section['style'] : {}}>
-            <div className="container">
+            <div className="container" style={'container-style' in props.section ? props.section['container-style'] : {}}>
                 {'header-text' in props.section ? <SubHeader header={props.section['header-text']} style={props.section['header-style']}/> : ''}
                 <div className="block-2">
                     {'first' in props.section ? <Block block={props.section['first']}/> : ''}
