c5472f468ae27db8b7bf39a55377bba0e66428ba
zachjbrowning <zachjbrowning@outlook.com>
2020-05-24 12:11:00 +1000

    deployable

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const DIST_DIR = path.join(__dirname, '../dist'); 
 const HTML_FILE = path.join(DIST_DIR, 'index.html'); 
 app.use(express.static(DIST_DIR)); 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
  res.sendFile(HTML_FILE); 
 });
 app.listen(port, function () {
